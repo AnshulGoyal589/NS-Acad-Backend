fcb39562b1d70d6b1684c6353e19fd485b8dc13e
Ishant Garg <119870492+Ishant-garg@users.noreply.github.com>
2025-06-22 18:07:07 +0530

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -8,7 +8,7 @@ const passport = require('passport');
 require('dotenv').config();
 
 const corsOptions = {
-  origin: 'http://localhost:5173'
+  origin: process.env.FRONTEND_URL || 'http://localhost:5173'
 };
 
 
